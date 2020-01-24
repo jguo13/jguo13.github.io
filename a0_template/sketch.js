5bb7b5a17022130df36dd9270436450afe1fa553
Joyce Guo <joyce.guo2@jetblue.com>
2020-01-24 10:44:14 -0500

    data viz

---
+++ a0_template/sketch.js
@@ -0,0 +1,46 @@
+let space = 4;
+function setup() {
+	createCanvas(800,600); // make an HTML canvas element width x height pixels
+
+
+}
+
+
+function draw() {
+	stroke('#222222')
+	background(200);
+	//second background
+	for (let i = 0; i < 60; i++) {
+		strokeWeight(3)
+		stroke(0,0,255,150)
+		line(1 + i * space, 500 - i * 4, 1 + i * space, 600);
+	}
+	//second hand
+	strokeWeight(3)
+	stroke(0,0,255,150)
+	line(second() * space, 500 - second() * 4, second() * space, 600);
+	//minute background
+	for (let i = 0; i < 60; i++) {
+		strokeWeight(5)
+		stroke(225,204,0,150)
+		line(20 + i * (space + 2), 0, 20 + i * (space  + 2), 400 - i * 6);
+	}
+	//minute progress bar
+	for (let i = 0; i < minute(); i++) {
+		strokeWeight(5)
+		stroke(825,104,0,150)
+		line(20 + i * (space + 2), 0, 20 + i * (space + 2), 400-i*6);
+	}
+	//minute hand
+	strokeWeight(3)
+	stroke('red')
+	line(20+minute()*(space+2), 0, 20+minute()*(space+2), 400-minute()*6);
+	//hour bars
+	for (let i = 0; i < hour(); i++) {
+		stroke(100 + i * (space + 20),0 + i * (space + 15),255 + i * (space + 20),150)
+		strokeWeight(30)
+		line(400 + i * (space + 30), 0, 400 + i * (space + 30), 600);
+	}
+}
+
+
